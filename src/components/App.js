c558486eab5bdc402fc7396ac6a64a2c951edb2b
Gian Ramirez <gece.ramirez@gmail.com>
2019-03-04 17:55:59 -0500

    chore(CODE BASE): code base for ui kit

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import Title from './atoms/Title';
+// import '../../static/css/fonts.css';
+
+const App = () => <Title>ENANO RCTMRE</Title>;
+
+export default App;
---
+++ src/components/atoms/Title/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export default styled.h1`
+    font-family: Lato, Arial;
+    font-size: 33px;
+    font-weight: 700;
+    color: #414042;
+`;
